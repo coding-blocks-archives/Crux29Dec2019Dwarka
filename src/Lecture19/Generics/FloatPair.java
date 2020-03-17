5b05ed3a804ef6618893260ae209cb26d4ebbe4c
rishab29 <rishab.kapoor.29@gmail.com>
2020-03-17 14:15:26 +0530

    Code For Lecture 19 and 20

---
+++ src/Lecture19/Generics/FloatPair.java
@@ -0,0 +1,7 @@
+package Lecture19.Generics;
+
+// Problem - could not reuse data types
+public class FloatPair {
+	float one;
+	float two;
+}
---
+++ src/Lecture20/client.java
@@ -0,0 +1,31 @@
+package Lecture20;
+
+public class client {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		HashTable<String, Integer> map = new HashTable<>(4);
+		map.put("India", 300);
+		map.put("Aus", 200);
+		map.display();
+		map.put("China", 350);
+		map.display();
+		map.put("Pak", 50);
+	//	map.display();
+//		map.display();
+		map.put("UK", 220);
+//		map.display();
+		map.put("USA", 150);
+		map.display();
+//		map.put("India",325);
+//		map.display();
+//		
+//		System.out.println(map.get("India"));
+//		System.out.println(map.get("USA"));
+//		System.out.println(map.get("Nepal"));
+////		
+//		System.out.println(map.remove("Pak"));
+//		map.display();
+	
+	}
+}
